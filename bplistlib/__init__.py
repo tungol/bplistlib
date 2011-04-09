3590f62845a2747eb6b45f6274abc33ffe25a547
tungolcraft <tungolcraeft@gmail.com>
2011-04-09 15:07:25 -0400

    code blocks

--- bplistlib/__init__.py
+++ bplistlib/__init__.py
@@ -9,34 +9,36 @@ optional keyword argument, binary.
 
 To write a plist use one of:
 
-writePlist(root_object, path_or_file)
-writePlistToString(root_object)
+    writePlist(root_object, path_or_file) 
+    writePlistToString(root_object) 
 
 Called like this, these functions will write an xml plist using plistlib.
 To write a binary plist, use one of:
 
-writePlist(root_object, path_or_file, binary=True)
-writePlistToString(root_object, binary=True)
+    writePlist(root_object, path_or_file, binary=True)
+    writePlistToString(root_object, binary=True)
 
 To read a plist use one of:
 
-readPlist(path_or_file)
-readPlistFromString(data)
+    readPlist(path_or_file)
+    readPlistFromString(data)
 
 Like this, these functions will attempt to determine whether the plist is
 binary or xml. If you know you have a binary plist, you can use one of:
 
-readPlist(path_or_file, binary=True)
-readPlistFromString(data, binary=True)
+    readPlist(path_or_file, binary=True)
+    readPlistFromString(data, binary=True)
 
 If you know you have an xml plist, you can use:
 
-readPlist(path_or_file, binary=False)
-readPlistFromString(data, binary=False)
+    readPlist(path_or_file, binary=False)
+    readPlistFromString(data, binary=False)
 
 
 Known issues:
-The actual plist format is more restrictive than is enforced here.
+The actual plist format is more restrictive than is enforced here. In 
+particular, key values in a plist dictionary must be strings, which is not 
+enforced. There may be other unenforced restrictions, be reasonable.
 """
 
 
